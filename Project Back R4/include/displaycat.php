ffa416e13a1cc50be550330c256e13476b54ab79
walaasalah123321 <Walaasalah123321@gmail.com>
2023-11-03 20:50:09 +0200

    task car project for R4

--- Project Back R4/include/displaycat.php
+++ Project Back R4/include/displaycat.php
@@ -1,12 +1,20 @@
 <?php include_once("admin/include/conn.php");
-$result=getRows('category');
+try{
+   $Sql="SELECT `catagory`, COUNT(car.id) AS num FROM `category` INNER JOIN `car` ON car.cat_id= category.id AND car.active = 1 
+   GROUP BY category.catagory " ;
+   $stm=$conn->prepare($Sql);
+   $stm->execute(); 
+   $result=$stm->fetchAll();
+}
+catch(PDOException $e){
+   echo "Connection failed: " . $e->getMessage();
+ }	
 ?>
 <div class="sidebar-box">
               <div class="categories">
-                <h3>Categories</h3>
-                <li><a href="#">Creatives <span>(12)</span></a></li>
+                <h3>Categories </h3>
                 <?php foreach($result as $row){?>
-                    <li><a href="#"><?php echo $row["catagory"]?> <span>(22)</span></a></li>
+                    <li><a href="#"><?php echo $row["catagory"]?> <span><?php echo $row["num"]?></span></a></li>
                  <?php }?>   
                
               </div>
